baec94ee0d05133b5e5c43058a33a7c6eae6497e
JuanJimenezGarcia <juanjimenezgarcia5@gmail.com>
2021-03-05 21:25:42 +0100

    Introducimos funcionalidad de tratamiento de datos

--- function.R
+++ function.R
@@ -11,3 +11,66 @@ lectura_datos <- function(){
   df_venta_diaria <- read.csv("data/venta_diaria.csv")
   return(df_venta_diaria)
 }
+
+
+#tratamiento_datos ----
+#Realiza el tratamiento de los datos. Orden, imputar NA y crear variable venta
+#
+#Recibe dataframe
+#
+#Devuelve dataframe con el tratamiento realizado
+tratamiento_datos <- function(df_venta_diaria){
+  df_venta_diaria <- ordenar_df(df_venta_diaria,"DIA")
+  df_venta_diaria <- imputar_missings_df(df_venta_diaria)
+  df_venta_diaria["VENTA"] <- df_venta_diaria["UNIDADES"] * df_venta_diaria["PRECIO"]
+  return(df_venta_diaria)
+}
+
+
+#ordenar_df ----
+#Ordena un dataframe en base a la columna que se le indique
+#
+#Recibe dataframe y nombre de columna por la que ordenar
+#
+#Devuelve dataframe ordenado
+ordenar_df <- function(df_venta_diaria,columna){
+  df_venta_diaria <- df_venta_diaria[order(df_venta_diaria[columna]),]
+  row.names(df_venta_diaria) <- NULL
+  return(df_venta_diaria)
+}
+
+
+#imputar_missings_df ----
+#Imputa missings en todas las columnas de un dataframe
+#
+#Recibe dataframe
+#
+#Devuelve dataframe con la imputacion realizada
+imputar_missings_df <- function(df_venta_diaria){
+  message("NAs antes de la imputacion")
+  print(lapply(lapply(df_venta_diaria,is.na),sum))
+  df_venta_diaria <- lapply(X = df_venta_diaria,FUN = imputar_missings_vector)
+  df_venta_diaria <- as.data.frame(df_venta_diaria)
+  message("NAs tras la imputacion")
+  print(lapply(lapply(df_venta_diaria,is.na),sum))
+  return(df_venta_diaria)
+}
+
+
+#imputar_missings_vector ----
+#Imputamos missings en el vector recibido. Si es numerico inserta la media,
+#si no la moda.
+#
+#Recibe vector
+#
+#Devuelve vector con la imputacion realizada
+imputar_missings_vector <- function(vector){
+  if(is.numeric(vector)){
+    vector[is.na(vector)] <- mean(vector,na.rm=TRUE)
+  } else {
+    uniqv <- unique(vector[!is.na(vector)])
+    moda <- uniqv[which.max(tabulate(match(vector[!is.na(vector)], uniqv)))]
+    vector[is.na(vector)] <- moda
+  }
+  return(vector)
+}
--- main.R
+++ main.R
@@ -1,6 +1,9 @@
 
-source("src/functions.R")
-
+source("function.R")
 
 #1 Leemos el dato ----
 df_venta_diaria <- lectura_datos()
+
+#2 Tratamiento del dato ----
+df_venta_diaria <- tratamiento_datos(df_venta_diaria)
+
